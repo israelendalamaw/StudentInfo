944db1f38aac3be7cc10919c4115da3a31808bfc
NOBODY <israelterfeisrael@gmail.com>
2022-05-04 01:04:18 +0300

    a working website bad sign up and not-so-good sign in page
    Signed-off-by: NOBODY <israelterfeisrael@gmail.com>

--- Project_2/settings.py
+++ Project_2/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'home'
